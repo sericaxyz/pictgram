d2195f0e9b20c82696c3fbf3eb751f6cefbe51f7
千田昭博 <sericaxyz@gmail.com>
2019-08-24 13:57:02 +0900

    Rails04メールアドレス正規表現追加

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,8 @@
 class User < ApplicationRecord
     validates :name, presence: true
+    
     validates :email, presence: true
+    validates :email, format:{with:/\A\S+@\S+\.\S+\z/ }
     
     validates :password, length: { in: 8..32}
     validates :password, format:{with:/([0-9].*[a-zA-Z]|[a-zA-Z].*[0-9])/ }
