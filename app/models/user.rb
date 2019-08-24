87929c8327cd7c35cc6d110adadc2afc2c43275a
千田昭博 <sericaxyz@gmail.com>
2019-08-24 12:33:03 +0900

    Rails04パスワードの文字数制限を追加

---
+++ app/models/user.rb
@@ -0,0 +1,8 @@
+class User < ApplicationRecord
+    validates :name, presence: true
+    validates :email, presence: true
+    
+    validates :password, length:{in:8..32}
+    
+    has_secure_password
+end
