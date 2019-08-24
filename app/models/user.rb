4da6dc80cf0763c4e56d000e77ca1516f424001d
千田昭博 <sericaxyz@gmail.com>
2019-08-24 13:35:07 +0900

    Rails04パスワードをアルファベット数字の混合に

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,7 +2,8 @@ class User < ApplicationRecord
     validates :name, presence: true
     validates :email, presence: true
     
-    validates :password, length:{in:8..32}
+    validates :password, length: { in: 8..32}
+    validates :password, format:{with:/([0-9].*[a-zA-Z]|[a-zA-Z].*[0-9])/ }
     
     has_secure_password
 end
